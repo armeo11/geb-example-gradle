d9bd0a69980183467ce4e58e41e8a3a2a7d9c349
Varokas Panusuwan <varokas@panusuwan.com>
2013-06-18 17:06:10 -0700

    Bumps up wrapper version. Only firefox for testing.

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -5,7 +5,6 @@
 */
 
 
-import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 
 waiting {
@@ -13,19 +12,7 @@ waiting {
 }
 
 environments {
-	
-	// run via “./gradlew chromeTest”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chrome {
-		driver = { new ChromeDriver() }
-	}
-	
-	// run via “./gradlew firefoxTest”
-	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
 	firefox {
 		driver = { new FirefoxDriver() }
 	}
-
 }
-
-// To run the tests with all browsers just run “./gradlew test”
